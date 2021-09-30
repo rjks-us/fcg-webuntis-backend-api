fb86cd6c0dd2009004d57226e6a914a6d089d994
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-09-30 17:16:12 +0200

    entry point

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const fs = require('fs');
+const path = require('path');
+
+const morgan = require('morgan');
+
+const config = require('./config.json')
+const app = express();
+
+app.use((err, req, res, next) => {
+    if(err.status === 400) return res.status(err.status).json({status: err.status, message: 'Invalid body, please check your json syntax'});
+    return next(err);
+});
+
+app.use('/v1', require('./api/v1/api'));
+
+/**
+ * @async
+ * @description Executes the entry point of application
+ */
+const init = async () => {
+    console.log(`[INFO] Application is starting...\n`);
+    console.log(`[INFO] FCG-Webuntis-API by Robert J. Kratz`);
+    console.log(`[INFO] You found an error? robert.kratz03@gmail.com\n`);
+    console.log(`[INFO] Date: ${new Date()}`);
+    console.log(`[INFO] Node Version: ${process.version}\n`);
+}
+
+app.use(morgan('combined', { stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })}))
+
+init().then(() => {
+    app.listen(config.port, () => {
+        console.log(`[INFO] Webserver started on http://localhost://$${config.port}`);
+    })
+})
