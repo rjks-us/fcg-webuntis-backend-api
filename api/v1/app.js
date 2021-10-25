c614dbb598cd47cbfbb4d6fb5e28b0a352259777
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-10-25 21:42:35 +0200

    router fix

--- api/v1/app.js
+++ api/v1/app.js
@@ -15,11 +15,28 @@ app.get('/holidays', async (req, res) => {
     res.status(200).json(result(200, `List of all ${classes.length} upcoming holidays`, classes));
 })
 
+app.get('/timegrid', async (req, res) => {
+    const classes = await utils.getTimeGrid();
+    res.status(200).json(result(200, `Current Timegrid`, classes));
+})
+
 app.get('/subjects/:class', async (req, res) => {
     if(!req.params.class) res.status(400).json(result(400, `Invalid Request`));
 
-    const sub = await utils.getAllSubjects(req.params.class, new Date('10.25.2021'), new Date('10.29.2021'));
-    res.status(200).json(result(200, `List of all ${sub.length} active subjects`, sub));
+    var now = new Date();
+    
+    const sub = await utils.getAllSubjects(req.params.class, new Date(`${now.getUTCMonth()}.01.${now.getFullYear()}`), new Date(`${now.getUTCMonth()}.29.${now.getFullYear()}`));
+    res.status(200).json(result(200, `List of all ${sub.length} active subjects in the current month (1.${now.getUTCMonth()} - 29.${now.getUTCMonth()})`, sub));
+})
+
+app.get('/subjectsList/:class', async (req, res) => {
+    if(!req.params.class) res.status(400).json(result(400, `Invalid Request`));
+
+    var now = new Date();
+
+    const sub = await utils.getAllSubjectsOrdered(req.params.class, new Date(`${now.getUTCMonth()}.01.${now.getFullYear()}`), new Date(`${now.getUTCMonth()}.29.${now.getFullYear()}`));
+
+    res.status(200).json(result(200, `List of all active subjects in the current month (1.${now.getUTCMonth()} - 29.${now.getUTCMonth()})`, sub));
 })
 
 app.get('/timetable/:class/:year/:month/:day', async (req, res) => {
@@ -30,5 +47,14 @@ app.get('/timetable/:class/:year/:month/:day', async (req, res) => {
     res.status(200).json(result(200, `List of all ${timetable.length} subjects`, timetable));
 })
 
+app.post('/timetable/:class/:year/:month/:day', async (req, res) => {
+    if(!req.params.class || !req.body.filter || !req.body.filter instanceof Array) res.status(400).json(result(400, `Invalid Request`));
+
+    if(req.body.filter.length == 0) res.status(200).json(result(200, `List of all 0 subjects`, []));
+
+    const timetable = await utils.getTimeTableWithFilter(req.params.class, new Date(req.params.month + '/' + req.params.day + '/' + req.params.year), req.body.filter);
+
+    res.status(200).json(result(200, `List of all ${timetable.length} subjects`, timetable));
+})
 
 module.exports = app;
