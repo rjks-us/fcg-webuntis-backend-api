9ec393171e594871612bc30882733b7a24b7413d
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-10-25 21:37:36 +0200

    implemented dynamic morgan

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const {result} = require('./structure');
 
 const app = express();
 
-const now = Date.now();
+const date = Date.now();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use((err, req, res, next) => {
     if(err.status === 400) return res.status(err.status).json(result(err.status, `Invalid body, please check your json syntax`));
@@ -27,22 +30,28 @@ app.use('/', require('./api/application'));
  * @description Executes the entry point of application
  */
 const init = async () => {
+    try {await fs.promises.access('logs');} catch (error) {await fs.promises.mkdir('logs');}
+
+    fs.writeFile(__dirname + `/logs/${date}.log`, '', {flag: 'wx', encoding: 'utf8'}, (err) => {
+        if(err) throw new Error(err);
+    })
+
+    app.use(morgan('combined', { stream: fs.createWriteStream(path.join(__dirname, `/logs/${date}.log`), { flags: 'a' })}));
+
     console.log(`[INFO] Application is starting...\n`);
     console.log(`[INFO] FCG-Webuntis-API by Robert J. Kratz`);
     console.log(`[INFO] You found an error? robert.kratz03@gmail.com\n`);
     console.log(`[INFO] Date: ${new Date()}`);
     console.log(`[INFO] Node Version: ${process.version}\n`);
-
-    app.use(morgan('combined', { stream: fs.createWriteStream(path.join(__dirname, '/logs/access.log'), { flags: 'a' })}))
 }
 
 init().then(() => {
-    app.listen(config.port, () => {
-        console.log(`[INFO] Webserver started on http://localhost://:${config.port}`);
-
-        untis.login().then(() => {
-            console.log('[INFO] Connected to WebUntis Backend API');
-            //untis.update(); //Starts requesting intervall
-        }).catch(err => console.error(err));
-    })
+    untis.login().then(() => {
+        console.log('[INFO] Connected to WebUntis Backend API');
+        //Webserver starts when connected to WebUntis API
+        app.listen(config.port, () => {
+            console.log(`[INFO] Webserver started on http://localhost:${config.port}/`);
+        })
+        require('./untis/cacher').update();
+    }).catch(err => console.error(err));
 });
