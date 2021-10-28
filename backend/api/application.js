6e1c19f7ab1dbf35faedd7e0545afe3656c4fb74
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-10-28 22:24:56 +0200

    changed import of package

--- backend/api/application.js
+++ backend/api/application.js
@@ -5,7 +5,7 @@ const config = require('../config.json');
 const {result} = require('../structure');
 
 app.get('/version', (req, res) => {
-    const package = require('../package.json');
+    const package = require('../../package.json');
 
     return res.status(200).json(
         result(200, `${package.name} running on version ${package.version}`, {
