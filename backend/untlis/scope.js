657a5561895a602ad548afd35c4d6868a89054ed
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-10-31 15:42:14 +0100

    init permission system

---
+++ backend/untlis/scope.js
@@ -0,0 +1,77 @@
+const SCOPES = require('../../scopes.json').scopes;
+
+/**
+ * 
+ * @param {Array} targetscope 
+ * @param {Array} userscopes 
+ * @returns Boolean
+ */
+ const canModify = (targetscope, userscopes) => {
+    for (let i = 0; i < userscopes.length; i++) {
+        if(!findScope(targetscope[i])) return false;
+        if(findScope(targetscope[i]).inherit.includes(userscopes[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * 
+ * @param {Int} id || {String} name
+ * @returns JSON
+ */
+const findScope = (id) => {
+    for(let i = 0; i < SCOPES.length; i++) {
+        if(SCOPES[i].id == id || SCOPES[i].name == id) {
+            return SCOPES[i];
+        }
+    }
+    return 0;
+}
+
+/**
+ * Checks if a user has a scope
+ * @param {Array} scopes 
+ * @param {ID} find 
+ * @returns Boolean
+ */
+const hasScope = (scopes, find) => {
+    for (let i = 0; i < scopes.length; i++) {
+        const s = scopes[i];
+        if(s === find) return true;
+        if(findScope(s).inherit.includes(find)) return true
+    }
+    return false;
+}
+
+/**
+ * Checks if a user has a scope
+ * @param {Array} scopes 
+ * @param {ID} find 
+ * @returns Boolean
+ */
+ const hasScopePrototype = (scopes, find) => {
+    return scopes.includes(find);
+}
+
+/**
+ * Checks if a user has a scope
+ * @param {Array} scopes 
+ * @param {Scope} scope 
+ * @returns Boolean
+ */
+const hasPermission = (scopes, scope) => {
+    for (let i = 0; i < scopes.length; i++) {
+        if(scopes[i].id == scope) return true;
+    }
+    return false;
+}
+
+module.exports = {
+    canModify: canModify,
+    hasScope: hasScope,
+    hasScopePrototype: hasScopePrototype,
+    findScope: findScope,
+    hasPermission: hasPermission
+}
