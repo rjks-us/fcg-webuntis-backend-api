c518addd95614d89d0a59a093a990995e00381cc
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-11-01 21:47:37 +0100

    react template

---
+++ webinterface/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Login from './components/Login';
+
+//import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<Login />, document.getElementById('root'));
+
+//reportWebVitals();
